3f6304b5c08d025f1a2e96ac53353421bfbdd830
Jovan Milanovic <milanovic97jovan@gmail.com>
2025-02-23 18:54:39 +0100

    feat: added message on submit

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,16 @@
 function sendMail(event) {
   event.stopPropagation();
   var recaptchaResponse = grecaptcha.getResponse();
+  var messageBox = document.getElementById("form-response-message");
+
   if (!recaptchaResponse) {
-    alert("Per favore completa la verifica reCAPTCHA.");
+    messageBox.innerHTML = "<p>Per favore completa la verifica reCAPTCHA.</p>";
+    messageBox.style.backgroundColor = "#ffdddd";
+    messageBox.style.color = "#d8000c";
+    messageBox.style.display = "block";
     return;
   }
+
   var params = {
     "request": document.getElementById("request").value,
     "name": document.getElementById("name").value,
@@ -13,14 +19,29 @@ function sendMail(event) {
     "message": document.getElementById("message").value,
     "g-recaptcha-response": recaptchaResponse,
   };
+
   const TEMPLATE_ID = "template_wavbo3i";
   const SERVICE_ID = "service_dzj1bll";
-  emailjs.send(SERVICE_ID, TEMPLATE_ID, params).then((res) => {
-    document.getElementById("request").value = "Seleziona il tipo di servizio";
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("message").value = "";
-  })
+
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, params)
+    .then((res) => {
+      document.getElementById("request").value = "Seleziona il tipo di servizio";
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("message").value = "";
+      
+      messageBox.innerHTML = "<p style='margin: 0'>Il tuo messaggio è stato inviato con successo!</p>";
+      messageBox.style.backgroundColor = "#d4edda";
+      messageBox.style.color = "#155724";
+      messageBox.style.display = "block";
+    })
+    .catch((err) => {
+      messageBox.innerHTML = "<p style='margin: 0' >Si è verificato un errore. Riprova più tardi.</p>";
+      messageBox.style.backgroundColor = "#ffdddd";
+      messageBox.style.color = "#d8000c";
+      messageBox.style.display = "block";
+    });
+
   grecaptcha.reset();
 }
